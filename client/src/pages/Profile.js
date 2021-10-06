691fb407072068ea8d92b5d2724ace3d34fed0ed
its-nobe <aabhay_rana_@outlook.com>
2021-10-06 18:46:46 +0530

    Edit post & Change Password

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,53 +1,67 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import axios from "axios";
+import { AuthContext } from "../helpers/AuthContext";
 
 function Profile() {
-  let { id } = useParams();
-  let history = useHistory();
-  const [username, setUsername] = useState("");
-  const [listOfPosts, setListOfPosts] = useState([]);
-  useEffect(() => {
-    axios.get(`http://localhost:1001/auth/basicInfo/${id}`).then((response) => {
-      setUsername(response.data.username);
-    });
-    axios.get(`http://localhost:1001/posts/byUserId/${id}`).then((response) => {
-      setListOfPosts(response.data);
-    });
-  }, []);
-  return (
-    <div className="profilePageContainer">
-      <div className="basicInfo">
-        <h1> Username: {username}</h1>
-      </div>
-      <div className="listOfPosts">
-        {listOfPosts.map((value, key) => {
-          return (
-            <div key={key} className="post">
-              <div className="title">{value.title}</div>
-              <div
-                className="body"
-                onClick={() => {
-                  history.push(`/post/${value.id}`);
-                }}
-              >
-                {value.postText}
-              </div>
-              <div className="footer">
-                <div className="username">
-                  <b>@</b>
-                  {value.username}
-                </div>
-                <div className="buttons">
-                  <label>{value.Likes.length}</label>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+	let { id } = useParams();
+	let history = useHistory();
+	const [username, setUsername] = useState("");
+	const [listOfPosts, setListOfPosts] = useState([]);
+	const { authState } = useContext(AuthContext);
+
+	useEffect(() => {
+		axios.get(`http://localhost:1001/auth/basicInfo/${id}`).then((response) => {
+			setUsername(response.data.username);
+		});
+
+		axios.get(`http://localhost:1001/posts/byUserId/${id}`).then((response) => {
+			setListOfPosts(response.data);
+		});
+	}, []);
+
+	return (
+		<div className="profilePageContainer">
+			<div className="basicInfo">
+				<h1> Username: {username}</h1>
+				{authState.username === username && (
+					<button
+						onClick={() => {
+							history.push("/changepassword");
+						}}
+					>
+						Change Password
+					</button>
+				)}
+			</div>
+			<div className="listOfPosts">
+				{listOfPosts.map((value, key) => {
+					return (
+						<div key={key} className="post">
+							<div className="title">{value.title}</div>
+							<div
+								className="body"
+								onClick={() => {
+									history.push(`/post/${value.id}`);
+								}}
+							>
+								{value.postText}
+							</div>
+							<div className="footer">
+								<div className="username">
+									<b>@</b>
+									{value.username}
+								</div>
+								<div className="buttons">
+									<label>{value.Likes.length}</label>
+								</div>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default Profile;
--- server/routes/Users.js
+++ server/routes/Users.js
@@ -6,43 +6,63 @@ const { validateToken } = require("../middlewares/AuthMiddleware");
 const { sign } = require("jsonwebtoken");
 
 router.post("/", async (req, res) => {
-  const { username, password } = req.body;
-  bcrypt.hash(password, 10).then((hash) => {
-    Users.create({
-      username: username,
-      password: hash,
-    });
-    res.json("SUCCESS");
-  });
+	const { username, password } = req.body;
+	bcrypt.hash(password, 10).then((hash) => {
+		Users.create({
+			username: username,
+			password: hash,
+		});
+		res.json("SUCCESS");
+	});
 });
 
 router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
+	const { username, password } = req.body;
 
-  const user = await Users.findOne({ where: { username: username } });
+	const user = await Users.findOne({ where: { username: username } });
 
-  if (!user) res.json({ error: "User Doesn't Exist" });
+	if (!user) res.json({ error: "User Doesn't Exist" });
 
-  bcrypt.compare(password, user.password).then((match) => {
-    if (!match) res.json({ error: "Wrong Username And Password Combination" });
+	bcrypt.compare(password, user.password).then((match) => {
+		if (!match) res.json({ error: "Wrong Username And Password Combination" });
 
-    const accessToken = sign(
-      { username: user.username, id: user.id },
-      "importantsecret"
-    );
-    res.json({ token: accessToken, username: username, id: user.id });
-  });
+		const accessToken = sign(
+			{ username: user.username, id: user.id },
+			"importantsecret"
+		);
+		res.json({ token: accessToken, username: username, id: user.id });
+	});
 });
 
 router.get("/auth", validateToken, (req, res) => {
-  res.json(req.user);
+	res.json(req.user);
 });
 
 router.get("/basicInfo/:id", async (req, res) => {
-  const id = req.params.id;
-  const basicInfo = await Users.findByPk(id, {
-    attributes: { exclude: ["password"] },
-  });
-  res.json(basicInfo);
+	const id = req.params.id;
+
+	const basicInfo = await Users.findByPk(id, {
+		attributes: { exclude: ["password"] },
+	});
+  
+	res.json(basicInfo);
 });
+
+router.put("/changepassword", validateToken, async (req, res) => {
+	const { oldPassword, newPassword } = req.body;
+	const user = await Users.findOne({ where: { username: req.user.username } });
+
+	bcrypt.compare(oldPassword, user.password).then(async (match) => {
+		if (!match) res.json({ error: "Wrong Password Entered!" });
+
+		bcrypt.hash(newPassword, 10).then((hash) => {
+			Users.update(
+				{ password: hash },
+				{ where: { username: req.user.username } }
+			);
+			res.json("SUCCESS");
+		});
+	});
+});
+
 module.exports = router;
