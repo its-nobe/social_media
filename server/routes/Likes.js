71ef0ef978bcabecf603ba491caa3dc445b22180
sid200128 <siddhant200128@gmail.com>
2021-10-09 09:47:44 +0530

    Function

--- server/routes/Likes.js
+++ server/routes/Likes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Likes } = require("../models");
+const { Likes, sequelize } = require("../models");
 const { validateToken } = require("../middlewares/AuthMiddleware");
 
 router.post("/", validateToken, async (req, res) => {
